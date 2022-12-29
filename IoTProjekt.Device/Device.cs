6c9e1362eb39fc21cd15ba71d2cf24796a4bdfc4
Michalo02 <m.poliwczak.02@gmail.com>
2022-12-29 23:16:27 +0100

    Dodaj pliki projektów.

---
+++ IoTProjekt.Device/Device.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System.Net.Mime;
+using System.Text;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Device
+{
+    public class Device
+    {
+        private readonly DeviceClient client;
+
+        public Device(DeviceClient deviceClient)
+        {
+            this.client = deviceClient;
+        }
+
+        #region Sending Messages
+
+        public async Task SendToHub(dynamic data, int delay)  //tablica z danymi i delay
+        {
+            var rnd = new Random();
+
+            Console.WriteLine($"Device sending messages to IoTHub...\n");
+            Console.WriteLine(data);
+
+            var dataString = JsonConvert.SerializeObject(data);
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+            eventMessage.ContentEncoding = "utf-8";
+            Console.WriteLine($"\t{DateTime.Now.ToLocalTime()}, Data: [{dataString}]");
+            await client.SendEventAsync(eventMessage);
+        }
+
+        #endregion Sending Messages
+
+        #region Receiving Messages
+
+        private async Task OnC2dMessageReceivedAsync(Message receivedMessage, object _)
+        {
+            Console.WriteLine($"\t{DateTime.Now}> C2D message callback - message received with Id={receivedMessage.MessageId}.");
+            PrintMessage(receivedMessage);
+
+            await client.CompleteAsync(receivedMessage);
+            Console.WriteLine($"\t{DateTime.Now}> Completed C2D message with Id={receivedMessage.MessageId}.");
+
+            receivedMessage.Dispose();
+        }
+
+        private void PrintMessage(Message receivedMessage)
+        {
+            string messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+            Console.WriteLine($"\t\tReceived message: {messageData}");
+
+            int propCount = 0;
+            foreach (var prop in receivedMessage.Properties)
+            {
+                Console.WriteLine($"\t\tProperty[{propCount++}> Key={prop.Key} : Value={prop.Value}");
+            }
+        }
+
+        #endregion Receiving Messages
+
+        #region Direct Methods
+
+        private async Task<MethodResponse> SendMessagesHandler(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+
+            var payload = JsonConvert.DeserializeAnonymousType(methodRequest.DataAsJson, new { data = default(dynamic), delay = default(int) });
+
+            await SendToHub(payload.data, payload.delay);
+
+            return new MethodResponse(0);
+        }
+
+        private static async Task<MethodResponse> DefaultServiceHandler(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\tMETHOD EXECUTED: {methodRequest.Name}");
+
+            await Task.Delay(1000);
+
+            return new MethodResponse(0);
+        }
+
+        #endregion Direct Methods
+
+        #region Device Twin
+
+        public async Task UpdateTwinAsync(dynamic productionRate, dynamic deviceErrors) //product i error
+        {
+            var twin = await client.GetTwinAsync();
+
+            Console.WriteLine($"\nInitial twin value received: \n{JsonConvert.SerializeObject(twin, Formatting.Indented)}");
+            Console.WriteLine();
+
+            var reportedProperties = new TwinCollection();
+            reportedProperties["ProductionRate"] = productionRate;
+            
+            var reportedProperties2 = new TwinCollection();
+            reportedProperties["DeviceErrors"] = deviceErrors;
+          
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties);
+            await client.UpdateReportedPropertiesAsync(reportedProperties2);
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            Console.WriteLine($"\tDesired property change:\n\t{JsonConvert.SerializeObject(desiredProperties)}");
+            Console.WriteLine("\tSending current time as reported property");
+            TwinCollection reportedProperties = new TwinCollection();
+            reportedProperties["DateTimeLastDesiredPropertyChangeReceived"] = DateTime.Now;
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        }
+
+        #endregion Device Twin
+
+        public async Task InitializeHandlers()
+        {
+            await client.SetReceiveMessageHandlerAsync(OnC2dMessageReceivedAsync, client);
+
+            await client.SetMethodHandlerAsync("SendMessages", SendMessagesHandler, client);
+            await client.SetMethodDefaultHandlerAsync(DefaultServiceHandler, client);
+
+            await client.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, client);
+        }
+    }
+}
---
+++ IoTProjekt/Console.cs
@@ -0,0 +1,67 @@
+﻿using Device;      //zmodyfikowac tak zeby laczylo z iotsim i ua i petla zeby 
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string deviceConnectionString = "HostName=PrzykladoweCentrumIoT.azure-devices.net;DeviceId=id-13102022;SharedAccessKey=TMC3nAGfSzGxOCy18yq8swX8hSOOquLTVCUDkqzP2YI=";
+
+        using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+        await deviceClient.OpenAsync();
+        var device = new Device(deviceClient);
+
+        await device.InitializeHandlers();
+        await device.UpdateTwinAsync();
+
+        Console.WriteLine($"Connection success!");
+
+        await device.SendMessages(10, 1000);
+
+        Console.WriteLine("Finished! Press Enter to close...");
+        Console.ReadLine();
+
+        using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+        {
+            client.Connect();
+
+            OpcReadNode[] commands = new OpcReadNode[]
+            {
+        new OpcReadNode("ns=2;s=Device 1/ProductionStatus", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/ProductionStatus"),
+        new OpcReadNode("ns=2;s=Device 1/ProductionRate"),
+        new OpcReadNode("ns=2;s=Device 1/WorkorderId", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/WorkorderId"),
+        new OpcReadNode("ns=2;s=Device 1/Temperature", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/Temperature"),
+        new OpcReadNode("ns=2;s=Device 1/GoodCount", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/GoodCount"),
+        new OpcReadNode("ns=2;s=Device 1/BadCount", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/BadCount"),
+        new OpcReadNode("ns=2;s=Device 1/DeviceError", OpcAttribute.DisplayName),
+        new OpcReadNode("ns=2;s=Device 1/DeviceError"),
+
+            };
+
+            IEnumerable<OpcValue> job = client.ReadNodes(commands);
+
+            foreach (var item in job)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            OpcNodeInfo machineNode = client.BrowseNode("ns=2;s=Device 1");
+
+            foreach (var chlidNode in machineNode.Children())
+            {
+                var isExecutable = chlidNode.Attribute(OpcAttribute.Executable)?.Value;
+                Console.WriteLine($"{chlidNode.DisplayName}{isExecutable}");
+            }
+        }
+    }
+}
